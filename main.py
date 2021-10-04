29f855561179429ce7254fc3e2ba8c14ccef7777
Matthijs van Wijngaarden <matthijs.van.wijngaarden@pangaea.nl>
2021-10-04 11:45:50 +0200

    Algo is tested and the result is that it would result in loss more often than profit

--- main.py
+++ main.py
@@ -1,83 +1,96 @@
 import collections
 from calculations import *
 
-file_name = "data/AVAXUSDT-1m-2021-01.csv"
+file_name = "data/AVAXUSDT-1m-2021-09.csv"
 
 # 1440 1m candles fit in one day
 previous_period_length = 1440
 
-# how many minutes to wait for a buy/sell
-minute_wait_time = 10
+for month in range(1, 10):
+    print(f"processing month: {month}")
+    # alter month of data file
+    file_name = list(file_name)
+    file_name[-5] = str(month)
+    file_name = "".join(file_name)
 
-# my_list will contain 1-minute candle info from the last 24 hours
-my_list = collections.deque([], previous_period_length)
+    # loop for multiple minute wait times
+    for minute_wait_time in range(5, 61):
 
-# flag for currently in buy or sell mode
-is_buying = False
+        # my_list will contain 1-minute candle info from the last 24 hours
+        my_list = collections.deque([], previous_period_length)
 
-# amounts of either currency in holdings
-base_amount = 100
-quote_amount = 0
+        # flag for currently in buy or sell mode
+        is_buying = False
 
-# transactions made
-transactions = []
+        # amounts of either currency in holdings
+        base_amount = 100
+        quote_amount = 0
 
-# counter to time a buy/sell
-counter = 0
+        # transactions made
+        transactions = []
 
-with open(file_name, 'r') as f:
+        # counter to time a buy/sell
+        counter = 0
 
-    # insert the first day of 1 minute candles to the list
-    for i in range(previous_period_length):
-        process_candle_data(f.readline(), my_list)
+        with open(file_name, 'r') as f:
+            # insert the first day of 1 minute candles to the list
+            for i in range(previous_period_length):
+                process_candle_data(f.readline(), my_list)
 
-    # calculate highest and lowest seen price
-    highest_price, highest_index, lowest_price, lowest_index = find_highest_and_lowest_price(my_list)
-
-    # go through every 1-minute candle to see if it breaches highest/lowest price
-    while True:
-        file_line = f.readline()
-
-        # if data_line is empty, reached end of file
-        if not file_line:
-            break
-
-        # process the csv file line into the list
-        process_candle_data(file_line, my_list)
-        highest_index += 1
-        lowest_index += 1
-
-        # check if minute_wait counter finished countdown, if so, execute a buy/sell order
-        if counter > 0:
-            counter -= 1
-            if counter == 0:
-                if not is_buying:
-                    base_amount, quote_amount = sell(base_amount, highest_price, my_list[0].open_time, transactions)
-                    is_buying = True
-                else:
-                    base_amount, quote_amount = buy(quote_amount, lowest_price, my_list[0].open_time, transactions)
-                    is_buying = False
-
-        # check if new candle exceeds highest price
-        if my_list[0].high >= highest_price:
-            highest_index = 0
-            highest_price = my_list[0].high
-
-            # sell logic
-            if not is_buying and counter == 0:
-                counter = minute_wait_time
-
-        # else, check if new candle exceeds lowest price
-        elif my_list[0].low <= lowest_price:
-            lowest_index = 0
-            lowest_price = my_list[0].low
-
-            # buy logic
-            if is_buying and counter == 0:
-                counter = minute_wait_time
-
-        # if the index exceeds list length, find the new highest/lowest index
-        if highest_index >= previous_period_length or lowest_index >= previous_period_length:
+            # calculate highest and lowest seen price
             highest_price, highest_index, lowest_price, lowest_index = find_highest_and_lowest_price(my_list)
+
+            # go through every 1-minute candle to see if it breaches highest/lowest price
+            while True:
+                file_line = f.readline()
+
+                # if data_line is empty, reached end of file
+                if not file_line:
+                    break
+
+                # process the csv file line into the list
+                process_candle_data(file_line, my_list)
+                highest_index += 1
+                lowest_index += 1
+
+                # check if minute_wait counter finished countdown, if so, execute a buy/sell order
+                if counter > 0:
+                    counter -= 1
+                    if counter == 0:
+                        if not is_buying:
+                            base_amount, quote_amount = sell(base_amount, my_list[0].open,
+                                                             my_list[0].open_time, transactions)
+                            is_buying = True
+                        else:
+                            base_amount, quote_amount = buy(quote_amount, my_list[0].open,
+                                                            my_list[0].open_time, transactions)
+                            is_buying = False
+
+                # check if new candle exceeds highest price
+                if my_list[0].high >= highest_price:
+                    highest_index = 0
+                    highest_price = my_list[0].high
+
+                    # sell logic
+                    if not is_buying and counter == 0:
+                        counter = minute_wait_time
+
+                # else, check if new candle exceeds lowest price
+                elif my_list[0].low <= lowest_price:
+                    lowest_index = 0
+                    lowest_price = my_list[0].low
+
+                    # buy logic
+                    if is_buying and counter == 0:
+                        counter = minute_wait_time
+
+                # if the index exceeds list length, find the new highest/lowest index
+                if highest_index >= previous_period_length or lowest_index >= previous_period_length:
+                    highest_price, highest_index, lowest_price, lowest_index = find_highest_and_lowest_price(my_list)
+
+            # record the final base_amount in the transaction list as the final result
+            with open('results.txt', 'a') as results_file:
+                results_file.write(f'{file_name} + {minute_wait_time} = {transactions[-1].base_amount}\n')
+
 
 pass
